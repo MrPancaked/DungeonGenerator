2a19b9dffe26c90a57924050be32614b763b695b
Mr. Pancake - René <renehammink@icloud.com>
2025-03-02 14:33:19 +0100

    Project setup

---
+++ Assets/Scripts/Lingus Bingus.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class LingusBingus : MonoBehaviour
+{
+    public bool bingus;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (bingus)
+        {
+            
+        }
+        
+    }
+}
